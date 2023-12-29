3c52bae2b79e41f358acb06d30dc19ed1383d73a
Achraf-haddar <achraf.haddar@supcom.tn>
2023-12-29 17:44:47 +0200

    Add Billboards page

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,11 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
+export default authMiddleware({
+  // To make public routes accessible from outside the browser without authentication
+  // Authentication and authorization is checked further in the api routes
+  publicRoutes: ["/api/:path*"]   
+});
  
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
